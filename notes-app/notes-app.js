32b932475813e863ed2fb03691a0b9f6d9cdf425
dhruvpathak9305 <dhruvpathak9305@gmail.com>
2020-01-21 18:22:41 +0530

    Date integration Part 1

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -8,11 +8,14 @@ renderNotes(notes, filters)
 
 document.querySelector('#create-note').addEventListener('click', function (e) {
     const id = uuidv4()
+    const timestamp = moment().valueOf()
 
     notes.push({
         id: id,
         title: '',
-        body: ''
+        body: '',
+        createdAt: timestamp,
+        updatedAt: timestamp
     })
     saveNotes(notes)
     location.assign(`/edit.html#${id}`)
@@ -33,19 +36,3 @@ window.addEventListener('storage', function (e) {
         renderNotes(notes, filters)
     }
 })
-
-// const now = moment()
-// now.subtract(1, 'week').subtract(20, 'days')
-// console.log(now.format('MMMM Do, YYYY'))
-// console.log(now.fromNow())
-// const nowTimestamp = now.valueOf()
-
-// console.log(moment(nowTimestamp).toString())
-
-// 1. Create a new moment
-// 2. Set the month, day, and year to your birthday
-// 3. Use format to print it in the following way: Jan 6, 1991
-
-const birthday = moment()
-birthday.year(1991).month(0).date(6)
-console.log(birthday.format('MMM D, YYYY'))
