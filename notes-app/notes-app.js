665020360fc9d6593029165d8d91e9446e497673
dhruvpathak9305 <dhruvpathak9305@gmail.com>
2020-01-21 16:10:26 +0530

    Local storage included

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -13,6 +13,18 @@ const filters = {
     searchText: ''
 }
 
+// const user = {
+//     name: 'Andrew',
+//     age: 27
+// }
+// const userJSON = JSON.stringify(user)
+// console.log(userJSON)
+// localStorage.setItem('user', userJSON)
+
+const userJSON = localStorage.getItem('user')
+const user = JSON.parse(userJSON)
+console.log(`${user.name} is ${user.age}`)
+
 const renderNotes = function (notes, filters) {
     const filteredNotes = notes.filter(function (note) {
         return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
@@ -33,13 +45,11 @@ document.querySelector('#create-note').addEventListener('click', function (e) {
     e.target.textContent = 'The button was clicked'
 })
 
-document.querySelector('#remove-all').addEventListener('click', function () {
-    document.querySelectorAll('.note').forEach(function( note) {
-        note.remove()
-    })
-})
-
 document.querySelector('#search-text').addEventListener('input', function (e) {
     filters.searchText = e.target.value
     renderNotes(notes, filters)
+})
+
+document.querySelector('#filter-by').addEventListener('change', function (e) {
+    console.log(e.target.value)
 })
